a51afc42bc45b1a48db5f05e56066debcd1bb482
Santino Mantineo <santinomantineo@gmail.com>
2024-02-20 23:48:18 -0300

    searchField front-end

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,13 +8,74 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+        searchField()
+      ],
+      ),
     );
+  }
+
+  Container searchField() {
+    return Container(
+        margin: EdgeInsets.only(top: 30, left: 20, right: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xff1D1617).withOpacity(0.11),
+              blurRadius: 40,
+              spreadRadius: 0.0
+            )
+          ]
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.all(15),
+            hintText: 'search Pancake',
+            hintStyle: TextStyle(
+              color: Color.fromARGB(255, 197, 195, 195),
+              fontSize: 15
+            ),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/search.svg'),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.2,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset('assets/icons/filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none
+              )
+          ),
+        ),
+      );
   }
 
   AppBar appBar() {
     return AppBar(
       title: const Text(
-        'C',
+        'Breakfast',
         style: TextStyle(
           color: Colors.black,
           fontSize: 18,
